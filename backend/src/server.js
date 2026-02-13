2cd4a9c35a3d3376d6c47d5276d0513e270341fb
Sai Sankar Madeti <saisankarmadeti@gmail.com>
2026-02-14 02:46:24 +0530

    Implement server setup with MongoDB connection and environment variables

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(
+    ()=>{
+        console.log('DB connected');
+        app.listen(PORT, ()=>{
+            console.log('Server on port:',PORT );
+        });
+    }
+)
+.catch((err)=>{
+    console.log('DB error:', err);
+});
